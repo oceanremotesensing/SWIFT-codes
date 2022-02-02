c102403d1e5e29b2f1b0b6d69dd6a07c45236f0c
edwinrainville <erainvil@uw.edu>
2022-02-02 11:30:08 -0800

    Updated Estimator

---
+++ microSWIFT/seaSurfaceDynamicsEstimator/dynamicsEstimator-test.py
@@ -0,0 +1,168 @@
+# import statements
+import numpy as np
+import matplotlib.pyplot as plt
+from seaSurfaceDynamicsEstimator import seaSurfaceDynamicsEstimator_linear
+
+class seaSurfaceDynamics:
+    def __init__(self, x, y, z, time, dt):
+        # Assign position of buoy at each point in space
+        self.time = time
+        self.dt = dt
+        self.x = x
+        self.y = y
+        self.z = z
+
+        # Compute Velocity arrays from positions
+        self.u = np.gradient(self.x) # cross-shore velocity, units are meters/sec
+        self.v = np.gradient(self.y) # alongshore velocity, units are meters/sec
+        self.w = np.gradient(self.z) # vertical velocity, units are meters/sec
+
+        # Compute Acceleration arrays from velocities
+        self.ax = np.gradient(self.u) # cross-shore acceleration, units are meters/sec^2
+        self.ay = np.gradient(self.v) # Along shore acceleration, units are meters/sec^2
+        self.az = np.gradient(self.w) # vertical acceleration, units are meters/sec^2
+
+        # Define the overall state
+        self.true_state = np.array([self.x, self.y, self.z, self.u, self.v, self.w, self.ax, self.ay, self.az])
+
+    def microSWIFTMeasurments(self):
+        ## ASSUMPTIONS:
+        # 1. All measurements are gaussian distributed with no bias 
+        # 2. All measurements are independent events
+
+        # Define microSWIFT instrument characteristics
+        gps_pos_std = 1         # lat, lon accuracy standard deviation, units are meters
+        gps_altitude_std = 1    # GPS alitude accuracy standard deviation, units are meters
+        gps_vel_std = 1         # GPS velocity standard deviation, units are meters/sec
+        imu_accel_std = 1       # IMU acceleration standard deviation, units are meters/sec^2
+        imu_gyro_str = 1        # IMU gyroscope standard deviation, units are degrees/sec
+
+        # GPS Measurements
+        # Compute measurments from random samples in a normal distribution with each instruments standard deviation
+        self.x_meas = np.random.normal(loc=0, scale=gps_pos_std, size=np.shape(self.x)) + self.x
+        self.y_meas = np.random.normal(loc=0, scale=gps_pos_std, size=np.shape(self.y)) + self.y
+        self.z_meas = np.random.normal(loc=0, scale=gps_altitude_std, size=np.shape(self.z)) + self.z
+
+        # Compute Velocity measurements
+        self.u_meas = np.random.normal(loc=0, scale=gps_vel_std, size=np.shape(self.u)) + self.u
+        self.v_meas = np.random.normal(loc=0, scale=gps_vel_std, size=np.shape(self.v)) + self.v
+        self.w_meas = np.gradient(self.z_meas)
+
+        # IMU Measurements
+        self.ax_meas = np.random.normal(loc=0, scale=imu_accel_std, size=np.shape(self.ax)) + self.ax
+        self.ay_meas = np.random.normal(loc=0, scale=imu_accel_std, size=np.shape(self.ay)) + self.ay
+        self.az_meas = np.random.normal(loc=0, scale=imu_accel_std, size=np.shape(self.az)) + self.az
+
+        # Define measured state
+        self.measured_state = np.array([self.x_meas, self.y_meas, self.z_meas, self.u_meas, self.v_meas, self.w_meas, self.ax_meas, self.ay_meas, self.az_meas])
+
+    def plotTrueState(self):
+        fig_simple_signals, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1)
+        # x and y position
+        ax1.plot(self.time, self.x)
+        ax1.plot(self.time, self.y)
+        ax1.set_ylabel('Position [meters]')
+
+        # Sea Surface Elevation signals
+        ax2.plot(self.time, self.z)
+        ax2.set_ylabel('Sea Surface Elevation [meters]')
+
+        # Velocity signals
+        ax3.plot(self.time, self.u)
+        ax3.plot(self.time, self.v)
+        ax3.plot(self.time, self.w)
+        ax3.set_ylabel('Velocity [m/s]')
+
+        # Acceleration Signals
+        ax4.plot(self.time, self.ax)
+        ax4.plot(self.time, self.ay)
+        ax4.plot(self.time, self.az)
+        ax4.set_ylabel('Acceleration [m/s^2]')
+        ax4.set_xlabel('Time [seconds]')
+
+        # Show the plot
+        plt.show()
+
+    def plotMeasuredState(self):
+        # Plot the measured values
+        fig_simple_measured, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1)
+        # x and y position
+        ax1.plot(self.time, self.x_meas)
+        ax1.plot(self.time, self.y_meas)
+        ax1.set_ylabel('Position Measured [meters]')
+
+        # Sea Surface Elevation signals
+        ax2.plot(self.time, self.z_meas)
+        ax2.set_ylabel('Sea Surface Elevation Measured [meters]')
+
+        # Velocity signals
+        ax3.plot(self.time, self.u_meas)
+        ax3.plot(self.time, self.v_meas)
+        ax3.set_ylabel('Velocity Measured [m/s]')
+
+        # Acceleration Signals
+        ax4.plot(self.time, self.ax_meas)
+        ax4.plot(self.time, self.ay_meas)
+        ax4.plot(self.time, self.az_meas)
+        ax4.set_ylabel('Acceleration [m/s^2]')
+        ax4.set_xlabel('Time [seconds]')
+
+        plt.show()
+
+    def compareEstimate2True(self):
+        fig_est, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1)
+        # x and y position
+        ax1.plot(self.time, self.state_est[0,:])
+        ax1.plot(self.time, self.state_est[1,:])
+        ax1.set_ylabel('Position [meters]')
+
+        # Sea Surface Elevation signals
+        ax2.plot(self.time, self.state_est[2,:])
+        ax2.set_ylabel('Sea Surface Elevation [meters]')
+                
+        # Velocity signals
+        ax3.plot(self.time, self.state_est[3,:])
+        ax3.plot(self.time, self.state_est[4,:])
+        ax3.set_ylabel('Velocity Measured [m/s]')
+
+        # Acceleration Signals
+        ax4.plot(self.time, self.state_est[6,:])
+        ax4.plot(self.time, self.state_est[7,:])
+        ax4.plot(self.time, self.state_est[8,:])
+        ax4.set_ylabel('Acceleration [m/s^2]')
+        ax4.set_xlabel('Time [seconds]')
+        plt.show()
+        
+
+def main():
+    '''
+    @edwinrainville
+
+    Description: This script is run to test the sea surface dynamics estimator algorithms that are developed.
+
+    '''
+
+    # Define time array for all tests
+    sampling_freq = 12 # Sampling frequency of the dataset, units are Hz
+    dt = 1/sampling_freq # time between samples, units are seconds
+    seconds = 1000
+    time = np.linspace(0,seconds, num=seconds*sampling_freq) # Time in seconds
+
+    ## Test 1 (Simple)- simple cosine function for surface with no noise and buoys moved in a straight line on surface
+    wave_period = 15 # Period of the test wave, units are seconds
+    x_simple_true = np.cos(((2*np.pi)/wave_period) * time) + np.arange(200, 500, step=(500-200)/len(time)) # straight line, units are meters
+    y_simple_true = np.cos(((2*np.pi)/wave_period) * time) + np.arange(100, 200, step=(200-100)/len(time)) # straight line, units are meters
+    z_simple_true = np.cos(((2*np.pi)/wave_period) * time) # cosine function, units are meters
+
+    # Instantiate simple sea class (singular cosine function)
+    simple_sea = seaSurfaceDynamics(x_simple_true, y_simple_true, z_simple_true, time, dt)
+    simple_sea.microSWIFTMeasurments()
+    simple_sea.plotTrueState()
+    simple_sea.plotMeasuredState()
+
+    # seaSurfaceDynamicsEstimator test
+    simple_sea.state_est = seaSurfaceDynamicsEstimator_linear(simple_sea.measured_state, simple_sea.dt)
+    simple_sea.compareEstimate2True()
+
+if __name__=='__main__':
+    main()
---
+++ microSWIFT/seaSurfaceDynamicsEstimator/seaSurfaceDynamicsEstimator.py
@@ -0,0 +1,93 @@
+import numpy as np
+from numpy.linalg import inv
+
+def seaSurfaceDynamicsEstimator_linear(measured_state, dt):
+    '''
+    @edwinrainville
+
+    Estimator Algorithm Overview(Adapted from "Bayesian and Kalman Filters"): 
+
+    Initialization
+    1. Initialize the state of the filter using first points measured
+    2. Initialize a belief in this state
+
+    Predict the next state
+    1. Use system behavior to predict the next time step - assume discretized newtonian motion 
+    2. Adjust belief in this prediction
+
+    Update the state with Measurements
+    1. Get a measurement of the state
+
+    '''
+
+    ## Initialization
+    # Define size of state and measurement vectors
+    dim_state = 9
+    dim_measurement = measured_state.shape[0]
+    dim_time = measured_state.shape[1]
+
+    # Define a state estimate array to be filled with values 
+    state_est = np.zeros((dim_state, dim_time))
+        
+    # Define initial uncertainty covariance
+    P = np.eye(dim_state)
+
+    # Define state transition matrix - Linear Newtonian system 
+    F = np.eye(dim_state)
+    F[0,3] = dt
+    F[1,4] = dt
+    F[2,5] = dt
+    F[3,6] = dt
+    F[4,7] = dt
+    F[5,8] = dt
+
+    # Define initial process uncertainty
+    Q = np.eye(dim_state)
+
+    # Define measurement function and uncertainty
+    H = np.zeros((dim_measurement, dim_state))
+    R = np.eye(dim_measurement)
+
+    # Define initial state as the first set of measurements
+    state_est[:,0] = measured_state[:,0]
+
+    # Kalman Filter
+    for k in np.arange(1,dim_time):
+        # Define the previous step 
+        previous_state = state_est[:,k-1]
+
+        ## Predict State Movement to Prior 
+        # x = Fx
+        state_prior = np.dot(F, previous_state)
+
+        # Compute State Covariance matrix
+        # P = FPF' + Q
+        P = np.dot(F, np.dot(P, F.T)) + Q
+
+        ## Update state movement from prior with measurement
+        # Compute Residual
+        # y = z - Hx
+        y = measured_state[:,k] - np.dot(H, state_prior)
+
+        # S = HPH' + R
+        S = np.dot(H, np.dot(P, H.T)) + R
+
+        # Compute Kalman Gain
+        # K = PH'inv(S)
+        K = np.dot(P, np.dot(H.T, inv(S)))
+
+        # Update next step with Kalman gain
+        # x = x + Ky
+        state_est_current = state_prior + np.dot(K, y)
+
+        # Save updated state into the state estimate matrix
+        state_est[:,k] = state_est_current
+
+        # Update state covariance matrix
+        # P = (I-KH)P(I-KH)' + KRK'
+        P = np.dot((np.eye(P.shape[0]) - np.dot(K, H)), np.dot(P,(np.eye(P.shape[0]) - np.dot(K, H)).T)) + np.dot(K, np.dot(R, K.T))
+
+        # Save the State Covariance matrix
+
+    return state_est
+    
